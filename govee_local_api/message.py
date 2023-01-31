cf5e977f99734b099c8bd71d05f9ef8ad8bb29e9
Andrea Pontecorvo <andrea.ponte1987@gmail.com>
2023-01-31 02:01:16 +0100

    Formatted

--- govee_local_api/message.py
+++ govee_local_api/message.py
@@ -5,139 +5,139 @@ import json
 
 
 class GoveeMessage:
-  command: str = None
+    command: str = None
 
-  def __init__(self, data: dict[str, Any]) -> None:
-    self._data = data
+    def __init__(self, data: dict[str, Any]) -> None:
+        self._data = data
 
-  def as_dict(self) -> dict[str, Any]:
-    return {
-      'msg': {
-        'cmd': self.command,
-        'data': self.data
-      }
-    }
+    def as_dict(self) -> dict[str, Any]:
+        return {"msg": {"cmd": self.command, "data": self.data}}
 
-  def as_json(self) -> str:
-    return json.dumps(self.as_dict())
+    def as_json(self) -> str:
+        return json.dumps(self.as_dict())
 
-  def __bytes__(self) -> bytearray|bytes:
-    return self.as_json().encode('utf-8')
+    def __bytes__(self) -> bytearray | bytes:
+        return self.as_json().encode("utf-8")
 
-  def __str__(self) -> str:
-    return self.as_json()
+    def __str__(self) -> str:
+        return self.as_json()
 
-  @property
-  def data(self) -> dict[str, Any]:
-    return self._data
+    @property
+    def data(self) -> dict[str, Any]:
+        return self._data
 
 
 class ScanMessage(GoveeMessage):
-  command = "scan"
+    command = "scan"
 
-  def __init__(self) -> None:
-    super().__init__({
-      'account_topic': 'reserve'
-    })
+    def __init__(self) -> None:
+        super().__init__({"account_topic": "reserve"})
+
 
 class StatusMessage(GoveeMessage):
-  command = "devStatus"
-  def __init__(self) -> None:
-    super().__init__({})
+    command = "devStatus"
+
+    def __init__(self) -> None:
+        super().__init__({})
+
 
 class OnOffMessage(GoveeMessage):
-  command = "turn"
+    command = "turn"
 
-  def __init__(self, on: bool) -> None:
-    super().__init__({
-      "value": int(on)
-    })
+    def __init__(self, on: bool) -> None:
+        super().__init__({"value": int(on)})
+
 
 class BrightnessMessage(GoveeMessage):
-  command = "brightness"
-  def __init__(self, brightness_pct: int) -> None:
-    super().__init__({
-      "value": brightness_pct
-    })
+    command = "brightness"
+
+    def __init__(self, brightness_pct: int) -> None:
+        super().__init__({"value": brightness_pct})
+
 
 class ColorMessage(GoveeMessage):
-  command = "colorwc"
-  def __init__(self, *, rgb: Tuple(int, int, int), temperature: int) -> None:
-    if rgb:
-      data = {
-        'color': {'r': rgb[0], 'g': rgb[1], 'b': rgb[2]},
-        'colorTemInKelvin': 0
-      }
-    elif temperature:
-      data = {
-        'color': {'r': 0, 'g': '0', 'b': 0},
-        'colorTemInKelvin': temperature
-      }
+    command = "colorwc"
 
-    super().__init__(data)
+    def __init__(self, *, rgb: Tuple(int, int, int), temperature: int) -> None:
+        if rgb:
+            data = {
+                "color": {"r": rgb[0], "g": rgb[1], "b": rgb[2]},
+                "colorTemInKelvin": 0,
+            }
+        elif temperature:
+            data = {
+                "color": {"r": 0, "g": "0", "b": 0},
+                "colorTemInKelvin": temperature,
+            }
+
+        super().__init__(data)
 
 
 class ScanResponse(GoveeMessage):
-  command = "scan"
-  
-  def __init__(self, data: dict[str, Any]) -> None:
-    super().__init__(data)
+    command = "scan"
 
-  @property
-  def device(self):
-    return self._data['device']
+    def __init__(self, data: dict[str, Any]) -> None:
+        super().__init__(data)
 
-  @property
-  def sku(self):
-    return self._data['sku']
+    @property
+    def device(self):
+        return self._data["device"]
 
-  @property
-  def ip(self):
-    return self._data['ip']
+    @property
+    def sku(self):
+        return self._data["sku"]
+
+    @property
+    def ip(self):
+        return self._data["ip"]
 
 
 class StatusResponse(GoveeMessage):
-  command = "devStatus"
+    command = "devStatus"
 
-  def __init__(self, data: dict[str, Any]) -> None:
-    super().__init__(data)
+    def __init__(self, data: dict[str, Any]) -> None:
+        super().__init__(data)
 
-  @property
-  def is_on(self) -> bool:
-    return bool(self._data['onOff'])
+    @property
+    def is_on(self) -> bool:
+        return bool(self._data["onOff"])
 
-  @property
-  def color(self) -> tuple(int, int, int):
-    color = self._data['color']
-    return (color['r'], color['g'], color['b'])
-  
-  @property
-  def brightness(self) -> int:
-    return self._data['brightness']
+    @property
+    def color(self) -> tuple(int, int, int):
+        color = self._data["color"]
+        return (color["r"], color["g"], color["b"])
 
-  @property
-  def color_temperature(self) -> int:
-    return self._data['colorTemInKelvin']
+    @property
+    def brightness(self) -> int:
+        return self._data["brightness"]
+
+    @property
+    def color_temperature(self) -> int:
+        return self._data["colorTemInKelvin"]
 
 
 class MessageResponseFactory:
-  def __init__(self) -> None:
-    self._messages: dict[str, GoveeMessage] = {ScanResponse, StatusResponse}
+    def __init__(self) -> None:
+        self._messages: dict[str, GoveeMessage] = {ScanResponse, StatusResponse}
 
-  def add_message(self, cls: GoveeMessage) -> None:
-    self._messages[cls.command] = cls
+    def add_message(self, cls: GoveeMessage) -> None:
+        self._messages[cls.command] = cls
 
-  def remove_message(self, command: str) -> None:
-    if (command in self._messages):
-      del self._messages[command]
+    def remove_message(self, command: str) -> None:
+        if command in self._messages:
+            del self._messages[command]
 
-  def create_message(self, data: bytes|bytearray|str) -> GoveeMessage:
-    msg_json = json.loads(data)
-    if 'msg' not in msg_json or 'cmd' not in msg_json['msg'] and 'data' not in msg_json['msg']:
-      return None
-    cmd = msg_json['msg']['cmd']
-    data = msg_json['msg']['data']
-    message = next(m for m in self._messages if m.command == cmd)
-    if not message:
-      return None
-    return message(data)
+    def create_message(self, data: bytes | bytearray | str) -> GoveeMessage:
+        msg_json = json.loads(data)
+        if (
+            "msg" not in msg_json
+            or "cmd" not in msg_json["msg"]
+            and "data" not in msg_json["msg"]
+        ):
+            return None
+        cmd = msg_json["msg"]["cmd"]
+        data = msg_json["msg"]["data"]
+        message = next(m for m in self._messages if m.command == cmd)
+        if not message:
+            return None
+        return message(data)
--- main.py
+++ main.py
@@ -2,42 +2,44 @@ import asyncio
 import functools
 
 from govee_local_api import GoveeController, GoveeDevice
-  
+
+
 def discovered_callback(device: GoveeDevice, is_new: bool):
-  if is_new:
-    print(f'Discovered: {device}. New: {is_new}')
+    if is_new:
+        print(f"Discovered: {device}. New: {is_new}")
+
 
 async def print_status(controller: GoveeController):
-  for device in controller.devices:
-    print(f'Status: {device}')
-  #await asyncio.sleep(5)
+    for device in controller.devices:
+        print(f"Status: {device}")
+    # await asyncio.sleep(5)
 
 
-async def main(controller: GoveeController):    
-  await controller.start()
-  await asyncio.sleep(5)
+async def main(controller: GoveeController):
+    await controller.start()
+    await asyncio.sleep(5)
 
-  device: GoveeDevice = controller.get_device_by_sku("H615A")
-  await device.turn_off()
-  await asyncio.sleep(5)
-  await print_status(controller)
+    device: GoveeDevice = controller.get_device_by_sku("H615A")
+    await device.turn_off()
+    await asyncio.sleep(5)
+    await print_status(controller)
 
 
-if __name__ == '__main__':
-  loop = asyncio.new_event_loop()
-  controller: GoveeController = GoveeController(
-      loop,
-      autodiscovery=True,
-      discovered_callback=discovered_callback,
-      evicted_callback=lambda device: print(f'Evicted {device}')
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    controller: GoveeController = GoveeController(
+        loop,
+        autodiscovery=True,
+        discovered_callback=discovered_callback,
+        evicted_callback=lambda device: print(f"Evicted {device}"),
     )
 
-  try:
-    loop.run_until_complete(main(controller))
-    loop.run_forever()
-  except KeyboardInterrupt:
-    pass
-  finally:
-    print("Closing Loop")
-    controller.clenaup()
-    loop.close()
+    try:
+        loop.run_until_complete(main(controller))
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("Closing Loop")
+        controller.clenaup()
+        loop.close()
