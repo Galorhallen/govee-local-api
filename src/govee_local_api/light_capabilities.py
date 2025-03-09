2674564313d9f0fed013d63600204c20cbbc4140
Galorhallen <andrea.ponte1987@gmail.com>
2025-03-09 12:46:29 +0100

    Add H6008

--- src/govee_local_api/light_capabilities.py
+++ src/govee_local_api/light_capabilities.py
@@ -113,6 +113,7 @@ ON_OFF_CAPABILITIES = create_with_capabilities(
 
 GOVEE_LIGHT_CAPABILITIES: dict[str, GoveeLightCapabilities] = {
     # Models with common features
+    "H6008": BASIC_CAPABILITIES,
     "H600D": BASIC_CAPABILITIES,
     "H6022": BASIC_CAPABILITIES,
     "H6042": create_with_capabilities(True, True, True, 5, True),
