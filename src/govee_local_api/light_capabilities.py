e154e93edc88b5fd6d909a811ab14248a14ddd67
Galorhallen <andrea.ponte1987@gmail.com>
2025-06-30 12:49:13 +0200

    Add H612C
    fix #153

--- src/govee_local_api/light_capabilities.py
+++ src/govee_local_api/light_capabilities.py
@@ -153,6 +153,7 @@ GOVEE_LIGHT_CAPABILITIES: dict[str, GoveeLightCapabilities] = {
     "H6117": BASIC_CAPABILITIES,
     "H612A": BASIC_CAPABILITIES,
     "H612B": BASIC_CAPABILITIES,
+    "H612C": create_with_capabilities(True, True, True, 0, True),
     "H612D": create_with_capabilities(True, True, True, 20, True),
     "H612F": create_with_capabilities(True, True, True, 5, True),
     "H6143": BASIC_CAPABILITIES,
