cf131e123bf719dc39b638549c9cf94c8094acea
Louish333 <128399125+Louish333@users.noreply.github.com>
2025-09-26 20:23:14 -0400

    Update H60A1 capabilities

--- src/govee_local_api/light_capabilities.py
+++ src/govee_local_api/light_capabilities.py
@@ -139,7 +139,7 @@ GOVEE_LIGHT_CAPABILITIES: dict[str, GoveeLightCapabilities] = {
     "H607C": BASIC_CAPABILITIES,
     "H608B": BASIC_CAPABILITIES,
     "H608D": BASIC_CAPABILITIES,
-    "H60A1": BASIC_CAPABILITIES,
+    "H60A1": create_with_capabilities(True, True, True, 13 , True),
     "H60A4": create_with_capabilities(True, True, True, 11, False),
     "H60A6": BASIC_CAPABILITIES,
     "H6072": BASIC_CAPABILITIES,
