e895d6263b2b069f36d6b15e231de96b063c9f14
Galorhallen <andrea.ponte1987@gmail.com>
2025-10-26 23:05:02 +0100

    Add H6020
    Fix #214

--- src/govee_local_api/light_capabilities.py
+++ src/govee_local_api/light_capabilities.py
@@ -115,6 +115,7 @@ ON_OFF_CAPABILITIES = create_with_capabilities(
 GOVEE_LIGHT_CAPABILITIES: dict[str, GoveeLightCapabilities] = {
     # Models with common features
     "H600D": BASIC_CAPABILITIES,
+    "H6020": create_with_capabilities(True, True, True, 0, True),
     "H6022": BASIC_CAPABILITIES,
     "H6039": create_with_capabilities(True, True, True, 12, True),
     "H6042": create_with_capabilities(True, True, True, 5, True),
