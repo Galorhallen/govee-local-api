baaebae99fa33e004fd858030485a2adca4ea8a8
Galorhallen <andrea.ponte1987@gmail.com>
2025-10-26 23:27:51 +0100

    Add H7076
    Fix #191

--- src/govee_local_api/light_capabilities.py
+++ src/govee_local_api/light_capabilities.py
@@ -262,6 +262,7 @@ GOVEE_LIGHT_CAPABILITIES: dict[str, GoveeLightCapabilities] = {
     "H7039": create_with_capabilities(True, True, True, 45, True),
     "H7052": create_with_capabilities(True, True, True, 15, True),
     "H7075": BASIC_CAPABILITIES,
+    "H7076": create_with_capabilities(True, True, True, 15, True),
     "H70A1": create_with_capabilities(True, True, True, 15, True),
     "H70A2": create_with_capabilities(True, True, True, 20, True),
     "H70A3": create_with_capabilities(True, True, True, 15, True),
